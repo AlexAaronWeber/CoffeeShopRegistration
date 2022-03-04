006b6f6b3886b0eedd77fdcf39ab52d7403f48eb
Alex <a2weber94@gmail.com>
2022-03-04 16:37:26 -0500

    initial registration commit

---
+++ CoffeeShopRegistration/Models/ProductContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CoffeeShopRegistration.Models
+{
+    public class ProductContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\sqlexpress; Database=CoffeeShop;Integrated Security=SSPI;");
+        }
+
+    }
+}
